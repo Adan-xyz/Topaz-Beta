c944199556fee1043453e4ed1f21383f2b244617
Adan-xyz <adansaprudin.xyz@gmail.com>
2025-01-31 13:46:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+const { AoiClient } = require("aoi.js");
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-});
-
-client.once('ready', () => {
-    console.log(`Bot ${client.user.tag} sudah online!`);
-});
-
-client.on('messageCreate', message => {
-    if (message.content === '!ping') {
-        message.reply('Pong!');
+const client = new AoiClient({
+    token: process.env.TOKEN, // Here goes the Token you copied earlier!
+    prefix: "!", // Here goes the prefix you want to use for your bot!
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("@aoijs/aoi.db"),
+        dbType: "KeyValue",
+        tables: ["main"],
+        securityKey: "a-32-characters-long-string-here"
     }
 });
+
+// Ping Command which responds when doing "!ping"
+client.command({
+    name: "ping",
+    code: `Pong! $pingms`
+});
+
+client.loadCommands("./commands");
