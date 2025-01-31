945fc1c0d96d64e28a68103e1e639f76e8969b5e
Adan-xyz <adansaprudin.xyz@gmail.com>
2025-01-31 13:34:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,3 @@ client.on('messageCreate', message => {
         message.reply('Pong!');
     }
 });
-
-client.login(process.env.DISCORD_TOKEN);
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
-
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
-});
-
-client.once('ready', () => {
-    console.log(`Bot ${client.user.tag} sudah online!`);
-});
-
-client.on('messageCreate', message => {
-    if (message.content === '!ping') {
-        message.reply('Pong!');
-    }
-});
-
-client.login(process.env.DISCORD_TOKEN);
